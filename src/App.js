08e9eef2b099d263612523cc9bdfb7a58963e0ce
Abhishek Chauhan <abhishek18chauhan@gmail.com>
2024-12-31 18:36:01 +0530

    Add Coming Soon Info

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{ marginTop: "8px", marginBottom: "8px" }}>Jai Mata Di !!!</p>
+        <img
+          src={"assets/images/sukharda_logo.png"}
+          className="App-logo"
+          alt="logo"
+        />
+        <p style={{ marginTop: "48px", fontSize: "48px", color: "red" }}>
+          Sukharda
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-link">We'll be here soon</p>
       </header>
     </div>
   );
