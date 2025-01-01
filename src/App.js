5691d9d20106c9153d6d506738976ab03ae73962
Abhishek Chauhan <abhishek18chauhan@gmail.com>
2025-01-01 16:51:49 +0530

    Add NavBar & Better Dashboard

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p style={{ marginTop: "8px", marginBottom: "8px" }}>Jai Mata Di !!!</p>
-        <img
-          src={"assets/images/sukharda_logo.png"}
-          className="App-logo"
-          alt="logo"
-        />
-        <p style={{ marginTop: "48px", fontSize: "48px", color: "red" }}>
+    <div className="w-full bg-bgLight">
+      <NavBar />
+
+      <div className="h-[90vh] w-full flex flex-col items-center justify-center">
+        <p className="text-xl font-bold text-amber-500">Jai Mata Di !!!</p>
+
+        <p className="text-6xl font-extrabold text-red-600">
           Sukharda
         </p>
-        <p className="App-link">We'll be here soon</p>
-      </header>
+        <div className="mt-12">
+          <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
+            <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+              Today's Efforts,
+            </span>{" "}
+            Future's Rewards
+          </h1>
+          <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+            We intend to make this world a better place unlocking long-term value and drive growth, riding on technology,
+            innovation, and unity
+          </p>
+        </div>
+        <p className="mt-24 text-2xl font-medium text-blue-400">We're coming soon ...</p>
+      </div>
     </div>
   );
 }
